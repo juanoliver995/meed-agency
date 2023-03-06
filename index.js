a97208f1b9d93d006e12fc4bd7c3e1e899c0fc59
Juan Oliver <juanoliver995@gmail.com>
2023-03-06 17:05:45 -0300

    agregue newsletter

--- index.js
+++ index.js
@@ -129,3 +129,23 @@ btnCloseMenu.addEventListener("click", () => {
     menuOpen.reverse()
 })
 
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'Subscribing...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_gfrty3y';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
